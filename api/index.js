0cbfc37c0ec720491ce5e521debc5db5d2cf9a05
徳山湧人 <tokuyama@tokuyamayuuninnoMacBook-Air.local>
2025-12-15 19:06:13 +0900

    stepB: echo text message

--- api/index.js
+++ api/index.js
@@ -9,22 +9,24 @@ export default async function handler(req, res) {
       return res.status(200).json({ ok: true });
     }
 
-    await fetch("https://api.line.me/v2/bot/message/reply", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
-      },
-      body: JSON.stringify({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: "text",
-            text: "【テスト】Webhookは正常に動いています",
-          },
-        ],
-      }),
-    });
+    if (event.message?.type === "text") {
+      await fetch("https://api.line.me/v2/bot/message/reply", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: "text",
+              text: `あなたは「${event.message.text}」と送りました`,
+            },
+          ],
+        }),
+      });
+    }
 
     return res.status(200).json({ ok: true });
   } catch (e) {
